743276972da82a9aff816b8001be99e1548d577a
Jakub Lisowski <Jlisowskyy@gmail.com>
2024-12-12 18:43:12 +0100

    Added small scripts

---
+++ scripts/display_move.py
@@ -0,0 +1,40 @@
+"""
+Author: Jakub Lisowski, 2024
+MIT License
+"""
+
+import sys
+
+BIT6_MASK = 0b111111
+FROM_MASK = BIT6_MASK
+TO_MASK = BIT6_MASK << 6
+PROMO_FLAG = 0b1000
+PROMO_MASK = PROMO_FLAG << 12
+
+
+def get_long_algebraic(move: int) -> str:
+    from_square = move & FROM_MASK
+    to_square = (move & TO_MASK) >> 6
+
+    from_file = from_square & 7
+    from_rank = from_square >> 3
+    to_file = to_square & 7
+    to_rank = to_square >> 3
+
+    if move & PROMO_FLAG:
+        raise NotImplementedError("Promotion not implemented")
+
+    return f"{chr(from_file + 97)}{from_rank + 1}{chr(to_file + 97)}{to_rank + 1}"
+
+
+def main(args: list[str]) -> None:
+    if len(args) != 1:
+        print("Usage: display_move.py <move integer>")
+        return
+
+    move = int(args[0])
+    print(get_long_algebraic(move))
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
---
+++ scripts/random_seed_gen.py
@@ -0,0 +1,84 @@
+"""
+Author: Jakub Lisowski, 2024
+MIT License
+"""
+import random
+import time
+import argparse
+
+def prepare_cpp_array_init_lis(strs: list[str]) -> str:
+    return "{\n" + ", ".join([f"\t{s}," for s in strs]) + "};\n"
+
+
+def prepare_cpp_array_init(strs: list[str], array_name: str, typename: str) -> str:
+    return f"{typename} {array_name}[] = {prepare_cpp_array_init_lis(strs)}"
+
+
+def generate_random_seed(count: int) -> list[int]:
+    random.seed(time.time())
+
+    return [random.randint(0, 2 ** 32 - 1) for _ in range(count)]
+
+
+# for each modulo value, generate 'count' random seeds
+def generate_pseudo_random_seed_modulo(count: int, modulo: int) -> list[int]:
+    if count == 0:
+        return []
+
+    random.seed(time.time())
+
+    rv = []
+    for expected_mod in range(modulo):
+        counter = count
+
+        while counter > 0:
+            seed = random.randint(0, 2 ** 32 - 1)
+            if seed % modulo == expected_mod:
+                rv.append(seed)
+                counter -= 1
+
+    return rv
+
+
+def random_seed_gen(count_random_seeds: int, count_modulo_seeds: int, modulo: int) -> None:
+    random_seeds = generate_random_seed(count_random_seeds)
+    modulo_seeds = generate_pseudo_random_seed_modulo(count_modulo_seeds, modulo)
+
+    print("Random seeds:")
+    print(prepare_cpp_array_init_lis([str(seed) for seed in random_seeds]))
+    print("Modulo seeds:")
+    print(prepare_cpp_array_init_lis([str(seed) for seed in modulo_seeds]))
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Generate random seeds and modulo random seeds.")
+    parser.add_argument(
+        "--count_random",
+        type=int,
+        default=0,
+        help="Number of random seeds to generate."
+    )
+    parser.add_argument(
+        "--count_modulo",
+        type=int,
+        default=0,
+        help="Number of modulo-specific seeds to generate for each modulo value."
+    )
+    parser.add_argument(
+        "--modulo",
+        type=int,
+        default=1,
+        help="Modulo value for generating modulo-specific seeds. Must be greater than 0."
+    )
+
+    args = parser.parse_args()
+
+    if args.modulo <= 0:
+        print("Error: The modulo value must be greater than 0.")
+        return
+
+    random_seed_gen(args.count_random, args.count_modulo, args.modulo)
+
+
+if __name__ == "__main__":
+    main()
